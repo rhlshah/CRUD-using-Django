2a8b16aa70d8e2d3b2bc845ca3825dd4e231f40f
visajshah <visaj.shah@gmail.com>
2022-02-03 14:52:58 +0530

    Setup Database

--- MoviesCRUD/MoviesCRUD/settings.py
+++ MoviesCRUD/MoviesCRUD/settings.py
@@ -73,11 +73,15 @@ WSGI_APPLICATION = 'MoviesCRUD.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'moviesdb',  
+        'USER':'root',  
+        'PASSWORD':'mysql',  
+        'HOST':'localhost',  
+        'PORT':'3306'  
+    }  
 }
 
 
