032a987c51ca71cd8173f7341ee93110cb415da4
visajshah <visaj.shah@gmail.com>
2022-02-04 10:47:11 +0530

    Create URL Paths for Views

--- MoviesCRUD/MoviesCRUD/urls.py
+++ MoviesCRUD/MoviesCRUD/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from movies import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("show", views.show),
+    path("edit/<int:id>", views.edit),
+    path("update/<int:id>", views.update),
+    path("destroy/<int:id>", views.destroy),
+    path("movie", views.movie)
 ]
